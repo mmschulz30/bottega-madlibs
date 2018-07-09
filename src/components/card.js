7656c54569229ceb87ecf8522572a0b3a0cf4e96
Makenna Schulz <mmschulz30@gmail.com>
2018-07-09 10:48:55 -0600

    finished clear form functionality

--- src/components/card.js
+++ src/components/card.js
@@ -3,6 +3,26 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+            nounTwo: '',
+            adjectiveThree: '',
+            celebTwo: '',
+            celebThree: '',
+            adjectiveFour: '',
+            nounThree: '',
+            celebFour: '',
+            adjectiveFive: '',
+            contentVisible: false
+}
+
 class Card extends Component {
 
     constructor() {
@@ -28,6 +48,8 @@ class Card extends Component {
             contentVisible: false
         }
 
+        this.state = INITIAL_STATE;
+
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
@@ -38,7 +60,12 @@ class Card extends Component {
 
     handleFormSubmit(event) {
         event.preventDefault()
-        this.setState({ contentVisible: !this.state.contentVisible })
+
+        if(this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({ contentVisible: true })
+        }
     }
 
     render() {
@@ -49,10 +76,10 @@ class Card extends Component {
             {title: 'Adjective', state: this.state.adjective, name: 'adjectiveOne'},
             {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
 
-            {title: 'Adjective', state: this.state.adjectiveTwo, name: 'AdjectiveTwo'},
+            {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
             {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
-            {title: 'Number', state: this.state.numberOne, name: 'NumberOne'},
-            {title: 'Number', state: this.state.NumberTwo, name: 'NumberTwo'},
+            {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
+            {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
 
             {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
             {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
@@ -72,7 +99,7 @@ class Card extends Component {
                 inputData.map(data => Input( (data), this.handleInputChange ))
                 }
                 </div>
-                <button type="submit">{!this.state.contentVisibel ? 'Generate Mad Lib' : 'Clear Form'}</button>
+                <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
